b62016cef42843f9d7e0b1b515e30c7de7e7f70a
Aynur Gatiyatullin <vildend@mail.ru>
2023-04-13 23:41:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,5 +6,5 @@ from scipy import stats
 chat_id = 897901830 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    pval = stats.kruskal(x, y, axis=1).pvalue
+    pval = stats.kruskal(x, y).pvalue
     return pval < 0.05
